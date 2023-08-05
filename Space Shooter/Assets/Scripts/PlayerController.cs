8b5bca53a703d080ccf3da4099cbf57e38ef820e
Diego Guilherme <diegoguilherme752@oultook.com>
2023-08-05 16:05:11 -0300

    feat: create enemy generator

--- Space Shooter/Assets/Scripts/PlayerController.cs
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -7,14 +7,21 @@ public class PlayerController : MonoBehaviour
     //[SerializeField] private float defaultTimer = 0.3f;
     //[SerializeField] private float timer = 1f;
 
-    [SerializeField] private GameObject shotPrefab;
-    [SerializeField] private GameObject explosionPrefab;
     [SerializeField] private Transform positionShotPlayer;
+    [SerializeField] private GameObject shotPrefab;
+    [SerializeField] private GameObject shotTwoPrefab;
+    [SerializeField] private GameObject explosionPrefab;
 
     private Rigidbody2D myRB;
     [SerializeField] private int life = 10;
     [SerializeField] private float speed = 5f;
     [SerializeField] private float speedShot = 10f;
+    [SerializeField] private float speedShotTwo = 16f;
+
+    [SerializeField] private float limitX;
+    [SerializeField] private float limitY;
+
+    [SerializeField] private int levelShot = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -40,14 +47,30 @@ public class PlayerController : MonoBehaviour
         mySpeed.Normalize();
 
         myRB.velocity = mySpeed * speed;
+
+        float checkingMyX = Mathf.Clamp(transform.position.x, -limitX, limitX);
+        float checkingMyY = Mathf.Clamp(transform.position.y, -limitY, limitY);
+
+        transform.position = new Vector3(checkingMyX, checkingMyY, transform.position.z);
     }
 
     void fire()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            GameObject shot = Instantiate(shotPrefab, positionShotPlayer.position, transform.rotation);
-            shot.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, speedShot);
+            if (levelShot <= 1)
+            {
+                CreateShot(shotPrefab, positionShotPlayer.position, speedShot);
+            } else
+            {
+                speedShot = 16f;
+
+                Vector3 positionShotLeft = new Vector3(positionShotPlayer.position.x - 0.45f, positionShotPlayer.position.y - 0.1f, 0f);
+                Vector3 positionShotRight = new Vector3(positionShotPlayer.position.x + 0.45f, positionShotPlayer.position.y - 0.1f, 0f);
+
+                CreateShot(shotTwoPrefab, positionShotLeft, speedShotTwo);
+                CreateShot(shotTwoPrefab, positionShotRight, speedShotTwo);
+            }
         }
         //else
         //{
@@ -60,6 +83,12 @@ public class PlayerController : MonoBehaviour
         //    }
 
         //}
+    }
+
+    private void CreateShot(GameObject shotPrefab, Vector3 position, float speedShot)
+    {
+        GameObject shot = Instantiate(shotPrefab, position, transform.rotation);
+        shot.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, speedShot);
     }
 
     public void loseLife(int damage)
